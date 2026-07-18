e007d495a708fad062a1c84cfaada0c226008537
veche <vecheruk.99@gmail.com>
2026-07-18 11:58:52 +0300

    Initial commit

---
+++ src/test/java/com/translatelab/backend/TranslationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translatelab.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
