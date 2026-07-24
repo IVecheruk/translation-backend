15c4ce3adaf6be8e8e5b3710af0607b2e16df043
veche <vecheruk.99@gmail.com>
2026-07-24 13:39:14 +0300

    Исправил пакеты

--- src/main/java/com/translatelab/backend/user/exception/UserNotFoundException.java
+++ src/main/java/com/translatelab/backend/user/exception/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package com.translatelab.backend.translation.exception;
+package com.translatelab.backend.user.exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException() {
